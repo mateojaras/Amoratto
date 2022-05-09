d3f5cf775fe9fbde3602fb9dbb444444967eec43
mateojaras <mateo.jaramillo@sofka.com.co>
2022-05-08 23:27:18 -0500

    Se hace responsive header y dashboard

---
+++ src/assets/styled-comp/navbar.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+interface openMenu {
+	open: boolean;
+}
+
+export const MobileIcon = styled.div`
+	display: none;
+	@media screen and (max-width: 960px) {
+		display: flex;
+		align-items: center;
+		cursor: pointer;
+
+		svg {
+			fill: #009288;
+			margin-right: 0.5rem;
+		}
+	}
+`;
+
+export const SubMenu = styled.div`
+	padding-bottom: 0.1rem;
+	border-top: 0.1rem solid #ededed;
+	@media screen and (max-width: 960px) {
+		position: absolute;
+		top: 120px;
+		left: ${({ open }: openMenu) => (open ? "0" : "-100%")};
+		border-top: none;
+	}
+`;
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { FaBars } from "react-icons/fa";
+import { MobileIcon, SubMenu } from "../assets/styled-comp/navbar";
 
 export const Header = () => {
+	const [showMenu, setShowMenu] = useState(false);
 	return (
 		<header className="anti_shadow">
 			<div className="shadow_line">
@@ -10,15 +14,18 @@ export const Header = () => {
 							src="	https://sarahssnacks.com/wp-content/uploads/2019/10/head-logo.png"
 							alt="Logo"
 						/>
+						<MobileIcon onClick={() => setShowMenu(!showMenu)}>
+							<FaBars />
+						</MobileIcon>
 					</div>
 
-					<div className="options_container container">
+					<SubMenu className="container" open={showMenu}>
 						<nav className="nav">
 							<a href="##">productos</a>
 							<a href="##">Nosotros</a>
 							<a href="##">Home</a>
 						</nav>
-					</div>
+					</SubMenu>
 				</div>
 			</div>
 		</header>
