c6a05374fd8f71b5fe33707d2d6f7db5c15603f3
mateojaras <mateo.jaramillo@sofka.com.co>
2022-06-02 18:16:42 -0500

    se crea responsividad del footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
+import {
+	BsFacebook,
+	BsInstagram,
+	BsTwitter,
+	BsTelephone,
+} from "react-icons/bs";
+import { FiMapPin } from "react-icons/fi";
+import { HiOutlineMail } from "react-icons/hi";
 
 const Footer = () => {
 	return (
@@ -17,9 +24,18 @@ const Footer = () => {
 					</div>
 				</div>
 				<div className="footer_information">
-					<h3> Calle del hospital • Mutata </h3>
-					<h3> leidytatianacardonaz@gmail.com</h3>
-					<h3>3105450117</h3>
+					<h3>
+						{" "}
+						<FiMapPin /> Calle del hospital • Mutata{" "}
+					</h3>
+					<h3>
+						{" "}
+						<HiOutlineMail /> leidytatianacardonaz@gmail.com
+					</h3>
+					<h3>
+						{" "}
+						<BsTelephone /> 3105450117
+					</h3>
 				</div>
 			</div>
 			<div className="footer_privacy">
